d290a3e6fa8a94271c1585a54fd1f20375d52324
Mike Smith <jyukumite@gmail.com>
2023-10-08 14:32:17 +1100

    Simple splash screen, works with app icon resource

---
+++ src/core/controls/Splash.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+
+namespace SehensWerte.Controls
+{
+    public class Splash
+    {
+        public static void Show(Icon icon, double seconds = 2.5)
+        {
+            Show(new Icon(icon, -1, -1).ToBitmap());
+        }
+
+        public static void Show(Bitmap bitmap, double seconds = 2.5)
+        {
+            var timer = new System.Windows.Forms.Timer();
+
+            var form = new Form();
+            form.Size = new Size(bitmap.Width, bitmap.Height);
+            form.FormBorderStyle = FormBorderStyle.None;
+            form.StartPosition = FormStartPosition.CenterScreen;
+            form.BackgroundImage = bitmap;
+            form.TransparencyKey = form.BackColor;
+            form.TopMost = true;
+
+            var pic = new PictureBox();
+            pic.Image = bitmap;
+            pic.Size = new Size(pic.Image.Width, pic.Image.Height);
+            pic.Location = new Point(0, 0);
+            pic.BackColor = Color.Transparent;
+            form.Controls.Add(pic);
+
+            timer.Interval = (int)(seconds * 1000);
+            EventHandler close = (s, e) => { timer.Stop(); form.Close(); };
+
+            pic.Click += close;
+            form.Click += close;
+            timer.Tick += close;
+            timer.Start();
+
+            form.Show();
+        }
+    }
+}
